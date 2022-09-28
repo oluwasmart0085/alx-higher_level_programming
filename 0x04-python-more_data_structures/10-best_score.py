2deec5982d7b9dcd9e67b26309b9e7214170e11b
oluwasmart <lebioluwasoji@gmail.com>
2022-09-28 11:05:17 -0700

    10-best_score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    keys = list(a_dictionary.keys())
+    values = list(a_dictionary.values())
+    return keys[values.index(max(values))]
