445fa3539fd2262b7379ee7334b6241e321b2461
oluwasmart <lebioluwasoji@gmail.com>
2022-10-11 12:41:45 -0700

    101-square py

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Define a square that is printable."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes the data."""
+        self.size = size
+        self.position = position
+
+    def __repr__(self):
+        """Print the square."""
+        string = ''
+        if self.__size == 0:
+            string += '\n'
+        else:
+            string += ('\n' * self.__position[1])
+            for i in range(self.__size):
+                string += (' ' * self.__position[0])
+                string += ('#' * self.__size + '\n')
+        return string[:-1]
+
+    @property
+    def size(self):
+        """Get the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size."""
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        """Get the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position."""
+        if type(value) != tuple or len(value) != 2 or type(value[0]) != int\
+           or type(value[1]) != int or value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size**2
+
+    def my_print(self):
+        """Print the square."""
+        if self.__size == 0:
+            print()
+        else:
+            print('\n' * self.__position[1], end='')
+            for i in range(self.__size):
+                print(' ' * self.__position[0], end='')
+                print('#' * self.__size)
