3764ddb28db9e23700a4060be5cf421614188eb0
oluwasmart <lebioluwasoji@gmail.com>
2022-12-20 21:51:38 -0800

    sorted js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
+// computes a dictionary of user ids by occurrence.
+const { dict } = require('./101-data');
+const newdict = {};
+for (const key in dict) {
+  if (dict[key] in newdict) {
+    newdict[dict[key]].push(key);
   } else {
-    newDict[dict[key]].push(key);
+    newdict[dict[key]] = [key];
   }
 }
-console.log(newDict);
+console.log(newdict);
